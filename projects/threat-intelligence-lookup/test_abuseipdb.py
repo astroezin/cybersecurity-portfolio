3ee362ffa15993c654418c22572253c440d504f4
Astroezin <rejinlimbu24@gmail.com>
2026-08-06 06:39:55 -0400

    Remove temporary API test scripts

--- projects/threat-intelligence-lookup/test_abuseipdb.py
+++
@@ -1,6 +0,0 @@
-from api.abuseipdb import lookup_ip
-from utils.formatter import format_abuseipdb_ip
-
-result = lookup_ip("8.8.8.8")
-
-print(format_abuseipdb_ip(result))
--- projects/threat-intelligence-lookup/test_vt.py
+++
@@ -1,6 +0,0 @@
-from api.virustotal import lookup_ip
-from utils.formatter import format_virustotal_ip
-
-result = lookup_ip("8.8.8.8")
-
-print(format_virustotal_ip(result))
